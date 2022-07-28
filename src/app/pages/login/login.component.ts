89fb89669519487412c317eaf40ac334fa4a0c61
Darwin Stalin Vilcachagua Ayala <dvilcachagua@hiper.com.pe>
2022-07-28 13:32:37 -0500

    inicio

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm = this.formBuilder.group({
+    email: '',
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+}
