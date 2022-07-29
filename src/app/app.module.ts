d49a947a7763c51873716f86932504d0f9d14464
Darwin Stalin Vilcachagua Ayala <dvilcachagua@hiper.com.pe>
2022-07-29 01:58:12 -0500

    lol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './pages/home/home.module';
+import { LoginModule } from './pages/login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
